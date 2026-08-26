f1a1798cb00aec58c47f3a5389b0462ab8cb6bf5
MehediVibeCoding <mehedivibecoding@gmail.com>
2026-08-26 20:17:15 +0600

    Update fonts.ts

--- app/fonts.ts
+++ app/fonts.ts
@@ -2,21 +2,21 @@ import { Playfair_Display, DM_Sans, Hind_Siliguri } from 'next/font/google';
 
 export const playfairDisplay = Playfair_Display({
   subsets: ['latin'],
-  weight: ['500', '600', '700', '800'],
+  weight: ['600', '700'],
   variable: '--font-display',
   display: 'swap',
 });
 
 export const dmSans = DM_Sans({
   subsets: ['latin'],
-  weight: ['400', '500', '600', '700', '800'],
+  weight: ['400', '500', '700'],
   variable: '--font-dm-sans',
   display: 'swap',
 });
 
 export const hindSiliguri = Hind_Siliguri({
   subsets: ['bengali', 'latin'],
-  weight: ['400', '500', '600', '700'],
+  weight: ['400', '600', '700'],
   variable: '--font-hind-siliguri',
   display: 'swap',
 });
