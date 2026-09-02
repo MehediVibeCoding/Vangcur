9b950d97de57d59430454970e470c359fad3ca4b
MehediVibeCoding <mehedivibecoding@gmail.com>
2026-09-02 18:38:19 +0600

    Update uiEvents.ts

--- lib/uiEvents.ts
+++ lib/uiEvents.ts
@@ -1,30 +1,18 @@
-// [REPLACE] ফাইলের পাথ: lib/uiEvents.ts
-
 export const OPEN_ACCOUNT_EVENT = 'vc:openAccount';
 export const OPEN_TRACK_ORDER_EVENT = 'vc:openTrackOrder';
 export const OPEN_OFFER_PAGE_EVENT = 'vc:openOfferPage';
-export const OPEN_INFO_EVENT = 'vc:openInfo';
 
 export const OPEN_CART_EVENT = 'vc:openCart';
 export const OPEN_WISHLIST_EVENT = 'vc:openWishlist';
 
-// প্রোডাক্ট কার্ড/প্রোডাক্ট পেজের হার্ট আইকনে ক্লিক করে wishlist-এ কিছু যোগ করার সাথে সাথে ডিসপ্যাচ হয়
 export const WISHLIST_FLY_EVENT = 'vc:wishlistFly';
-// উড়ন্ত হার্টটা Navbar আইকনে "হিট" করার মুহূর্তে ডিসপ্যাচ হয়
 export const WISHLIST_NAV_HIT_EVENT = 'vc:wishlistNavHit';
 
 export const OPEN_WAIT_OVERLAY_EVENT = 'vc:openWaitOverlay';
 export const SHOW_BG_CONFIRM_EVENT = 'vc:showBgConfirmPopup';
-export const GENERATE_INVOICE_EVENT = 'vc:generateInvoice';
 export const OPEN_MEMBERSHIP_EVENT = 'vc:openMembership';
-export const OPEN_ORDER_FORM_EVENT = 'vc:openOrderForm';
 export const SHOW_POST_RECEIVE_INFO_EVENT = 'vc:showPostReceiveInfo';
 
-// ফ্লোটিং শপিং কার্ট বটম শিট ওপেন করার ইভেন্ট
 export const OPEN_QUICK_CART_MODAL_EVENT = 'vc:openQuickCartModal';
-
-// দৈনিক ৩-অর্ডার লিমিট অতিক্রম করলে প্রিমিয়াম পপআপ ওপেন করার ইভেন্ট
 export const OPEN_ORDER_LIMIT_EVENT = 'vc:openOrderLimitModal';
-
-// ২০,০০০ টাকার বেশি অর্ডারের ক্ষেত্রে WhatsApp বাল্ক অর্ডার পপআপ ওপেন করার ইভেন্ট
 export const OPEN_BULK_ORDER_EVENT = 'vc:openBulkOrderModal';
